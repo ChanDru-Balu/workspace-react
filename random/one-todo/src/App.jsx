936b207a72c29111fd229711b0453961bc5786d9
ChanDru-Balu <prochandru@gmail.com>
2025-08-20 01:43:06 +0530

    use state logic

--- random/one-todo/src/App.jsx
+++ random/one-todo/src/App.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
+
+  const [ newItem , setNewItem ] = useState("Hai");
+  // setNewItem('hai')
+
   return (
     <>
       <h1>Todo</h1>
       <form>
         <div>
           <label htmlFor="item">New Item</label>
-          <input type="text" name="item" id="item" />
+          <input type="text" value={newItem} 
+          onChange={e=> {setNewItem(e.target.value)}} 
+          name="item" id="item" />
         </div>
         <button>Delete</button>
       </form>
@@ -15,7 +21,7 @@ function App() {
       <ul style={{listStyleType: 'none'}} >
         <li>
           <label >
-            <input type="checkbox" name="" id="" />
+            <input type="checkbox"/>
             Item 1
           </label>
           <button>Delete</button>
